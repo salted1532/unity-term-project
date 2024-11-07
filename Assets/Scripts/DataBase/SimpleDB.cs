52d1b7e96e0c188aceaa540d2ccea722aa59c42b
Salted1532 <salted1532@naver.com>
2024-11-07 20:32:26 +0900

    맵 조금 제작 및 DB연동 날짜 및 등수 추가 및 정렬버튼 추가
    등수랑 데이터 등록날짜 추가하고 등수, 날짜별로 정렬해주는 버튼 기능 구현

--- Assets/Scripts/DataBase/SimpleDB.cs
+++ Assets/Scripts/DataBase/SimpleDB.cs
@@ -1,15 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Data;
 using Mono.Data.Sqlite;
-using UnityEditor.MemoryProfiler;
 using TMPro;
 using UnityEngine.UI;
 
 public class SimpleDB : MonoBehaviour
 {
-
     private string dbName = "URI=file:UserData.db";
 
     public TMP_InputField idInput;
@@ -27,7 +26,6 @@ public class SimpleDB : MonoBehaviour
 
     private bool isClear = false;
 
-    // Start is called before the first frame update
     void Start()
     {
         Rankpage.SetActive(false);
@@ -40,10 +38,9 @@ public class SimpleDB : MonoBehaviour
         Displayuserdata();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(isClear == false)
+        if (isClear == false)
         {
             clearTime += Time.deltaTime;
 
@@ -59,9 +56,8 @@ public class SimpleDB : MonoBehaviour
 
             using (var command = connection.CreateCommand())
             {
-                command.CommandText = "CREATE TABLE IF NOT EXISTS userdata( id VARCHAR(20), review VARCHAR(30), cleartime INT(4));";
+                command.CommandText = "CREATE TABLE IF NOT EXISTS userdata(id VARCHAR(20), review VARCHAR(30), cleartime INT(4), cleardate VARCHAR(20), rank INT);";
                 command.ExecuteNonQuery();
-
             }
             connection.Close();
         }
@@ -69,49 +65,82 @@ public class SimpleDB : MonoBehaviour
 
     public void Adduserdata()
     {
-        using (var connection = new SqliteConnection(dbName)) 
-        { 
+        string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+
+        using (var connection = new SqliteConnection(dbName))
+        {
             connection.Open();
             using (var command = connection.CreateCommand())
             {
-
-                command.CommandText = "INSERT INTO userdata (id, review, cleartime) VALUES ('" + idInput.text + "', '" + reviewInput.text + "', '" + ((int)clearTime) + "');";
-
+                command.CommandText = "INSERT INTO userdata (id, review, cleartime, cleardate, rank) VALUES ('" + idInput.text + "', '" + reviewInput.text + "', '" + ((int)clearTime) + "', '" + currentDate + "', 0);";
                 command.ExecuteNonQuery();
-
             }
-            connection.Close ();
+            connection.Close();
         }
+
+        UpdateRanks();
         Displayuserdata();
         clearTime = 0;
+    }
+
+    public void UpdateRanks()
+    {
+        using (var connection = new SqliteConnection(dbName))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                // Order by cleartime ascending to assign ranks
+                command.CommandText = "SELECT id FROM userdata ORDER BY cleartime;";
+                List<string> ids = new List<string>();
+
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        ids.Add(reader["id"].ToString());
+                    }
+                    reader.Close();
+                }
+
+                // Update each user's rank based on their order in the list
+                int rank = 1;
+                foreach (var id in ids)
+                {
+                    command.CommandText = $"UPDATE userdata SET rank = {rank} WHERE id = '{id}';";
+                    command.ExecuteNonQuery();
+                    rank++;
+                }
+            }
+            connection.Close();
+        }
     }
 
     public void Displayuserdata()
     {
         userlist.text = "";
 
-        using ( var  connection = new SqliteConnection(dbName))
+        using (var connection = new SqliteConnection(dbName))
         {
-            connection.Open(); 
+            connection.Open();
 
             using (var command = connection.CreateCommand())
             {
-                command.CommandText = " SELECT * FROM userdata;";
+                command.CommandText = "SELECT * FROM userdata ORDER BY rank;";
 
                 using (IDataReader reader = command.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        userlist.text += "ID: " + reader["id"] + "\tcleartime: " + reader["cleartime"] + "초" + "\tReview: " + reader["review"] + "\n";
+                        userlist.text += "Rank: " + reader["rank"] + "\tID: " + reader["id"] + "\tcleartime: " + reader["cleartime"] + "초" + "\tReview: " + reader["review"] + "\tDate: " + reader["cleardate"] + "\n";
                     }
 
                     reader.Close();
-
                 }
             }
             connection.Close();
         }
-
     }
 
     public void Clearuserdata()
@@ -141,19 +170,47 @@ public class SimpleDB : MonoBehaviour
 
             using (var command = connection.CreateCommand())
             {
-                command.CommandText = " SELECT * FROM userdata ORDER BY cleartime;";
+                command.CommandText = "SELECT * FROM userdata ORDER BY rank;";
 
                 using (IDataReader reader = command.ExecuteReader())
                 {
                     int i = 1;
                     while (reader.Read())
                     {
-                        Ranklist.text +=  i + " 등 "+ "ID: " + reader["id"] + "\tcleartime: " + reader["cleartime"] + "초" + "\tReview: " + reader["review"] + "\n";
+                        Ranklist.text += i + " 등 " + "\tID: " + reader["id"] + "\tcleartime: " + reader["cleartime"] + "초" + "\tReview: " + reader["review"] + "\tDate: " + reader["cleardate"] + "\n";
                         i++;
                     }
 
                     reader.Close();
+                }
+            }
+            connection.Close();
+        }
+    }
 
+    public void DateSelectOn()
+    {
+        Rankpage.SetActive(true);
+        Ranklist.text = "";
+
+        using (var connection = new SqliteConnection(dbName))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT * FROM userdata ORDER BY cleardate;";
+
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    int i = 1;
+                    while (reader.Read())
+                    {
+                        Ranklist.text += "Rank: " + reader["rank"] + "\tID: " + reader["id"] + "\tcleartime: " + reader["cleartime"] + "초" + "\tReview: " + reader["review"] + "\tDate: " + reader["cleardate"] + "\n";
+                        i++;
+                    }
+
+                    reader.Close();
                 }
             }
             connection.Close();
@@ -167,7 +224,6 @@ public class SimpleDB : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        // 플레이어가 충돌할 때 문 열기
         if (other.gameObject.CompareTag("Player"))
         {
             Clearpage.SetActive(true);
