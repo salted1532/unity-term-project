eb5a1003a15e29a73860d89ec8c4e264c7a3278e
Salted1532 <salted1532@naver.com>
2024-11-27 22:31:55 +0900

    산탄총 코드에 빌드시 오류발생하는 코드 삭제

--- Assets/Scripts/Player/PlayerWeapon/PlayerWeaponShotGun.cs
+++ Assets/Scripts/Player/PlayerWeapon/PlayerWeaponShotGun.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
 using UnityEngine.Events;
-using static UnityEditorInternal.ReorderableList;
+
 public class PlayerWeaponShotGun : MonoBehaviour
 {
     public bool isReLoading;
